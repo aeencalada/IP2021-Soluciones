e7584b6b961ce44e3e1e9fab2adb8202471e55e4
aeencalada <aeencalada@gmail.com>
2021-12-08 01:06:13 -0500

    202112080700

--- 1erBim-ExamenParcial/TrianguloRectangulo3.java
+++ 1erBim-ExamenParcial/TrianguloRectangulo3.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class TrianguloRectangulo3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		short lado1, lado2, lado3;
+		byte lado1, lado2, lado3;
 
 		System.out.println("\nLADOS DE UN TRIÁNGULO RECTANGULO");
 		System.out.println("--------------------------------\n");
 		System.out.print("Ingrese el lado 1: ");
-		lado1 = sc.nextShort();
+		lado1 = sc.nextByte();
 		System.out.print("Ingrese el lado 2: ");
-		lado2 = sc.nextShort();
+		lado2 = sc.nextSByte();
 		System.out.print("Ingrese el lado 3: ");
-		lado3 = sc.nextShort();
+		lado3 = sc.nextByte();
 
 		if (Math.pow(lado1,2) == Math.pow(lado2,2) + Math.pow(lado3,2)) {
 			System.out.println("\nSI corresponden a los lados de un triángulo rectángulo");
