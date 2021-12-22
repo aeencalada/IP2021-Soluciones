088d60c4d15140399bb4135bb981539e191bc467
aeencalada <aeencalada@gmail.com>
2021-12-22 02:44:52 -0500

    20211222

--- Taller09/NumeroCapicua1.java
+++ Taller09/NumeroCapicua1.java
@@ -6,43 +6,30 @@ public class NumeroCapicua1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i, j, num, cociente, cant_digitos;
-        boolean capicua;
-        System.out.print("Ingrese un número entero positivo: ");
-        num = sc.nextInt();
-        // Contar dígitos
-        cociente = num;
-        cant_digitos = 0;
-        while (cociente > 0) {
-            cant_digitos++;
-            cociente = cociente / 10;
-        }
-        byte[] digitos = new byte[cant_digitos];
-        // Guardar dígitos en vector
-        cociente = num;
-        i = cant_digitos - 1;
-        while (cociente > 0) {
-            digitos[i] = (byte) (cociente % 10);
-            cociente = cociente / 10;
-            i--;
-        }
-        // Validar si es capicúa
-        i = 0;
-        j = cant_digitos - 1;
-        capicua = true;
-        while (i < j) {
-            if (digitos[i] != digitos[j]) {
-                capicua = false;
-                i = j;
+        int i, num, num_inv, cociente, res;
+
+        do {
+            System.out.print("Ingrese un número entero positivo: ");
+            num = sc.nextInt();
+            if (num <= 0) {
+                System.out.println("Dato no válido!");
             }
-            i++;
-            j--;
+        } while (num <= 0);
+
+        num_inv = 0;
+        cociente = num;
+
+        while (cociente > 0) {
+            res = cociente % 10;
+            num_inv = num_inv * 10 + res;
+            cociente = cociente / 10;
         }
-        if (capicua) {
+        
+        if (num == num_inv) {
             System.out.println("\nEl número " + num + " es capicúa");
         } else {
             System.out.println("\nEl número " + num + " no es capicúa");
         }
     }
-
+    
 }
--- Taller09/NumeroCapicua1b.java
+++ Taller09/NumeroCapicua1b.java
@@ -6,11 +6,10 @@ public class NumeroCapicua1b {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int i, num, num2, cociente, cant_digitos;
-
+        int i, j, num, cociente, cant_digitos;
+        boolean capicua;
         System.out.print("Ingrese un número entero positivo: ");
         num = sc.nextInt();
-
         // Contar dígitos
         cociente = num;
         cant_digitos = 0;
@@ -18,16 +17,28 @@ public class NumeroCapicua1b {
             cant_digitos++;
             cociente = cociente / 10;
         }
-
-        // Generar num2 sentido inverso
-        num2 = 0;
+        byte[] digitos = new byte[cant_digitos];
+        // Guardar dígitos en vector
         cociente = num;
-        for (i = cant_digitos - 1; i >= 0; i--) {
-            num2 = num2 + (int) Math.pow(10, i) * (cociente % 10);
+        i = cant_digitos - 1;
+        while (cociente > 0) {
+            digitos[i] = (byte) (cociente % 10);
             cociente = cociente / 10;
+            i--;
         }
-
-        if (num == num2) {
+        // Validar si es capicúa
+        i = 0;
+        j = cant_digitos - 1;
+        capicua = true;
+        while (i < j) {
+            if (digitos[i] != digitos[j]) {
+                capicua = false;
+                i = j;
+            }
+            i++;
+            j--;
+        }
+        if (capicua) {
             System.out.println("\nEl número " + num + " es capicúa");
         } else {
             System.out.println("\nEl número " + num + " no es capicúa");
